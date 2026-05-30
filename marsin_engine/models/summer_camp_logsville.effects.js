1184b852eefc8670d55de3c3b8cf381df296313c
Titanic's End <96307973+titanicsend@users.noreply.github.com>
2026-05-30 03:25:38 -0700

    PRE Logsville

--- marsin_engine/models/summer_camp_logsville.effects.js
+++ marsin_engine/models/summer_camp_logsville.effects.js
@@ -1,5 +1,5 @@
 // Auto-generated Companion Special Effects model — do not edit manually
-// Updated: 2026-05-28T19:11:42.230Z
+// Updated: 2026-05-30T01:10:33.124Z
 
 export const specialEffects = [
 ];
